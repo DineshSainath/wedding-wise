dbb8a27c2e3d0f4125dd1821846c33490c1a56dd
Dinesh Sainath <sainath.dinesh@gmail.com>
2024-08-01 17:13:50 +0530

    footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,9 +5,7 @@ function Footer() {
   return (
     <footer className="bg-light py-3 mt-auto">
       <Container>
-        <p className="text-center mb-0">
-          &copy; 2023 Wedding Wise. All rights reserved.
-        </p>
+        <p className="text-center mb-0">&copy; Wedding Wise</p>
       </Container>
     </footer>
   );
